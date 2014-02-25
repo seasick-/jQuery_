a7915eecd35238f78973e349c04d2e1236a55a73
= <=>
2014-02-25 03:44:05 -0800

    added array of ajax calls

---
+++ pass_Array_of_$.ajax.js
@@ -0,0 +1,10 @@
+
+var promises = [ 
+	$.ajax('http://google.com'), 
+	$.ajax('http://yahoo.com'), 
+	$.ajax('http://www.nytimes.com')
+];
+
+$.when.apply($, promises).then( function(result1, result2, result3){
+	console.log('All URLs fetched.'); }
+);
